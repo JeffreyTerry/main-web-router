58dd90793045929988739f8305e3aeb745cd40a0
Ali Hajimirza <a92hm@yahoo.com>
2014-02-03 23:00:35 -0600

    inital commit

---
+++ server.js
@@ -0,0 +1,41 @@
+var express = require('express'),
+    main = express();
+
+
+main.use(express.logger('dev'));  /* 'default', 'short', 'tiny', 'dev' */
+// SoBA Spring 2014 app
+try 
+{
+	var soba = require ('../soba');
+	main.use(express.vhost('54.84.50.34',soba));
+}
+catch (err)
+{
+	console.log('Failed to launch soba app: \n \t' + err.message);
+}
+
+// AliHM.net
+try
+{
+	var alihm = require('../alihm');
+	main.use(express.vhost('*.alihm.net', alihm));
+	main.use(express.vhost('alihm.net',alihm));
+}
+catch (err)
+{
+	console.log('Failed to launch alihm app: \n \t' + err.message);
+}
+
+// demo.sirenweather.com
+try
+{
+	sirenDemo = require('../siren-demo')
+	main.use(express.vhost('*.demo.sirenweather.com',sirenDemo));
+	main.use(express.vhost('demo.sirenweather.com',sirenDemo));
+}
+catch (err)
+{
+	console.log('Failed to launch demo.sirenweather app: \n \t' + err.message);
+}
+
+main.listen(8080);
